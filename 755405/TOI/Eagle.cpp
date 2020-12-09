bef539051e5216c1fb3b692cc786755d42619f01
Airq <a755405@stdmail.nssh.ntpc.edu.tw>
2020-12-09 21:54:34 +0800

    homework

---
+++ 755405/TOI/Eagle.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int chicknum, ghost, ghost1, round;
+	cin >> chicknum;
+	int a[chicknum];
+	for (int i = 0; i < chicknum; i++) {
+		cin >> a[i];
+	}
+	cin >> ghost;
+	cin >> round;
+	int b[round];
+	for (int j = 0; j < round; j++)	{
+		cin >> b[j];
+	}
+	for (int o = 0; o < round; o++) {
+		for (int p = 0; p < chicknum; p++) {
+			if (a[p] == b[o]) {
+				ghost1 = a[p];
+				a[p] = ghost;
+				ghost = ghost1;
+			}
+		}
+	}
+	for (int y = 0; y < chicknum; y++) {
+		cout << a[y] << " ";
+	}
+}
---
+++ 755405/TOI/Score.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int N, S, Q, sum;
+	cin >> N >> S;
+	int a[N];
+	for (int u = 0; u < N; u++) {
+		cin >> a[u];
+	}
+	cin >> Q;
+	int b[Q][N];
+	for (int i = 0; i < Q; i++) {
+		for (int j = 0; j < N; j++){
+			cin >> b[i][j];
+		}
+	}
+	for (int p = 0; p < Q; p++) {
+		sum = 0;
+		for (int o = 0; o < N; o++) {
+			if (a[o] == b[p][o]) {
+				sum = sum + S;		
+			}
+		}
+		cout << sum << "\n";
+	}
+}
+
